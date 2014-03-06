7356c981595e968d8a268a5d78262a45ef97d8b6
renansep <renan@Renans-MacBook-Pro-2.local>
2014-03-06 11:51:35 -0300

    White Background

---
+++ Source/MainMenuScene.h
@@ -0,0 +1,18 @@
+//
+//  MainMenuScene.h
+//  MatchX
+//
+//  Created by Renan Cargnin on 3/6/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface MainMenuScene : CCNode
+{
+    CCLabelTTF *highscoreLabel;
+}
+
+- (void)startGame:(id)sender;
+
+@end
---
+++ Source/MainMenuScene.m
@@ -0,0 +1,23 @@
+//
+//  MainMenuScene.m
+//  MatchX
+//
+//  Created by Renan Cargnin on 3/6/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "MainMenuScene.h"
+
+@implementation MainMenuScene
+
+- (void)didLoadFromCCB
+{
+    NSLog(@"Main Menu loaded.");
+}
+
+- (void)startGame:(id)sender
+{
+    NSLog(@"startGame");
+}
+
+@end
